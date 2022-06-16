9ae2047f300b88e4307573b0fa90acfe4ad1df5c
rolandexplore93 <roland2rule@gmail.com>
2022-06-16 20:15:23 +0100

    Implement Mongoose validation and install validator package

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,29 @@
 // import user model
 const User = require("../models/User")
 
+// handle errors
+const handleErrors = (err => {
+    console.log(err.message, err.code);
+    let errors = { email: '', password: ''}
+
+    // duplicate error code
+    if (err.code === 11000){
+        errors.email = "Email already exist in our database";
+        return errors
+    }
+
+    // errors validation
+    if (err.message.includes('user validation failed')){
+        console.log(Object.values(err.errors).forEach(({properties}) => {
+            // console.log(properties)
+            // access the error object and decide which value to update
+            errors[properties.path] = properties.message
+        }))
+    }
+
+    return errors
+})
+
 module.exports.signup_get = (req, res) => {
     res.render('signup')
 }
@@ -19,8 +42,10 @@ module.exports.signup_post = async (req, res) => {
         console.log(user)
     }
     catch(err){
-        console.log(err);
-        res.status(400).send("Error occurred! Please, check if your details are correct")
+        const error = handleErrors(err)
+        // res.status(400).send("Error occurred! Please, check if your details are correct")
+        res.status(400).json({ error })
+
     }
 }
 
--- models/User.js
+++ models/User.js
@@ -1,17 +1,19 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { isEmail } = require('validator')
 
 const userSchema = new Schema({
     email: {
         type: String,
-        required: true,
+        required: [true, 'Please enter your email'],
         unique: true,
-        lowercase: true
+        lowercase: true,
+        validate: [isEmail, 'Please enter a valid email address']
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Password cannot be empty'],
+        minlength: [6, 'Minimum password length is 6 characters']
     }
 }, { timestamps: true })
 
